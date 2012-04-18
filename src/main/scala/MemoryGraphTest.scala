1f6078f5ffbd1b9fdaf9a554cc9b18f811bc0bd8
Michael O. Church <michael.o.church@gmail.com>
2012-04-18 15:22:23 -0400

    Clean up MemoryGraphTest.scala

--- src/main/scala/MemoryGraphTest.scala
+++ src/main/scala/MemoryGraphTest.scala
@@ -2,30 +2,23 @@ object MemoryGraphTest {
   def runTests() = {
     TestEase.inTest()
 
+    // 1. Run the common graph tests.
     val graphTester = new GraphTest(MemoryGraph.basic)
     val graphs = graphTester.go()
+
+    // 2. Run some tests specific to mutable graphs.
+    def addNodeAndRetrieveIt() = {
+      val g = MemoryGraph.basic
+      val node1 = BaseNode("testType", Map("one" -> "1"))
+      val nodeId1 = g.addNode(node1)
+      assert(g.getNode(nodeId1) == Some(node1))
+    }
+
+    addNodeAndRetrieveIt()
   }
 
   def main(args:Array[String] = Array()) = {
     runTests()
     println("MemoryGraphTest: PASSED")
-  }
-}
-
-object Test {
-  def addNodeAndRetrieveIt() = {
-    val g = new MemoryGraph[BaseNode,BaseEdge]()
-    val node1 = BaseNode("testType", Map("one" -> "1"))
-    val nodeId1 = g.addNode(node1)
-    assert(g.getNode(nodeId1) == Some(node1))
-  }
-
-  def runTests() = {
-    addNodeAndRetrieveIt()
-  }
-
-  def main(args:Array[String]) = {
-    runTests()
-    println("Passed.")
   }
 }
